f5c937ba1d871a1e642adb43baed38a5f16b3f62
cgray425 <conall116@hotmail.com>
2014-08-20 14:59:33 +0100

    Day 1 Work

--- js/navigation.js
+++ js/navigation.js
@@ -21,7 +21,9 @@
   });
 
   function showCarDetails() {
-      
+      $("#dvPersonalDetails").hide();
+      $("#dvQuoteDetails").hide();
+      $("#dvCarDetails").show();
     // Hide the personal details section (dvPersonalDetails)
     // Hide the quote section (dvQuoteDetails)
     // Show the car details section (dvCarDetails)
@@ -29,8 +31,9 @@
   }
 
   function showPersonalDetails() {
-      // Hide the car details section (dvCarDetails)
-      // Hide the quote section (dvQuoteDetails)
+      $("#dvCarDetails").hide();
+      $("#dvQuoteDetails").hide();
+      $("#dvPersonalDetails").show();
       // Show the personal details section (dvPersonalDetails)
   }
 
@@ -43,8 +46,8 @@
   function getQuote() {
 
     // Perform validation to test that all data has been entered
-
-    if (/* Page is Valid */)
+/*
+    if ( Page is Valid )
     {
 
       // Get the values from the page elements that you need to create your JSON
@@ -52,12 +55,12 @@
       $.ajax({
           type: "GET",
           url: "http://localhost:53753/api/rating/CalculateRates",
-          data: { /* create JSON here */ }
+          data: { /* create JSON here / }
         }).done(function(msg) {
           // Put the return value into Label created on quote details
           // Hide the Car Details section
           // Display the quote details page
-      });
+      });*/
   }
 
 //################################# Helper Functions - look at these when validating and changing section #########################################
